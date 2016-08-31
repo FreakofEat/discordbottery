dd2a6e861092239aba4b249a3395355b878d6ad1
freakofeat <FreakofEat@users.noreply.github.com>
2016-08-31 17:48:51 -0400

    bad fix for aiohttp :(

--- cogs/queries.py
+++ cogs/queries.py
@@ -10,7 +10,6 @@ import threading
 
 # global vars
 js_driver = None
-session = aiohttp.ClientSession()
 
 
 class Queries:
@@ -65,22 +64,24 @@ class Queries:
         await self.bot.say(image_url)
 
     @commands.command()
-    async def copypasta(self, search=""):
+    async def   copypasta(self, search=""):
         """Pastes a random copypasta (copypasterino.me)
         add a query after to search"""
         pasta = ""
         if search != "":
             search_urlsafe = urllib.parse.quote_plus(search)
             url = 'http://copypasterino.me/search/' + search_urlsafe
-            async with session.get(url) as r:
-                json = await r.json()
-                pasta = json[random.randrange(0, len(json))]['pasta']
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as r:
+                    json = await r.json()
+                    pasta = json[random.randrange(0, len(json))]['pasta']
         else:
             url = 'http://copypasterino.me/static/all/hot/' + \
                   str(random.randint(1, 7))
-            async with session.get(url) as r:
-                json = await r.json()
-                pasta = json[random.randrange(0, len(json))]['pasta']
+            async with aiohttp.ClientSession() as session:
+                async with session.get(url) as r:
+                    json = await r.json()
+                    pasta = json[random.randrange(0, len(json))]['pasta']
         """
         url = 'http://copypasterino.me/general/hot/' + str(random.randint(1, 7))
         # print(url)
@@ -184,11 +185,11 @@ async def bing_img_search(query, safe=True, offset=0):
         search_q += '&Adult=%27Off%27'
     url = base_url + '&Query=' + search_q + \
         '&$top=1&$format=JSON&$skip=' + str(offset)
-
-    async with session.get(
-            url, auth=("", os.environ['BING_API_KEY'])) as response:
-        results = await response.json()
-        response.close()
+    async with aiohttp.ClientSession() as session:
+        async with session.get(
+                url, auth=("", os.environ['BING_API_KEY'])) as response:
+            results = await response.json()
+            response.close()
 
     image_url = results['d']['results'][0]['Image']
     if len(image_url) > 0:
@@ -206,8 +207,9 @@ async def after_the_deadline(query, type=0):
         url = 'http://service.afterthedeadline.com/checkGrammar'
     else:
         url = 'http://service.afterthedeadline.com/checkDocument'
-    async with session.post(url, data=data) as r:
-        response = await r.text()
+    async with aiohttp.ClientSession() as session:
+        async with session.post(url, data=data) as r:
+            response = await r.text()
     soup = BeautifulSoup(response, 'html.parser')
     output = ""
     for error in soup.find_all('error'):
